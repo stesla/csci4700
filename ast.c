2e042cc3383c52ff3e33da11d490c8b62ceb44f0
Samuel Tesla <samuel.tesla@gmail.com>
2008-09-24 19:06:11 -0500

    Re-arrange ast.c and get rid of switch statement.

--- ast.c
+++ ast.c
@@ -8,38 +8,7 @@ static const char *_node_type_strings[] =
    "AST_STRING_LITERAL"};
 #define NODE_STR(type) (_node_type_strings[(type)])
 
-static NODE *ast_create_identifier(va_list args);
-static NODE *ast_create_constant(va_list args);
-static NODE *ast_create_string_literal(va_list args);
-
-NODE *ast_create(NODE_TYPE type, ...)
-{
-  va_list args;
-  NODE *result = NULL;
-
-  va_start(args, type);
-  switch(type) {
-  case AST_CONSTANT:
-    result = ast_create_constant(args);
-    break;
-
-  case AST_IDENTIFIER:
-    result = ast_create_identifier(args);
-    break;
-
-  case AST_STRING_LITERAL:
-    result = ast_create_string_literal(args);
-    break;
-
-  default:
-    fprintf(stderr, "UNHANDLED CREATE: %s\n", NODE_STR(type));
-  }
-  va_end(args);
-
-  return result;
-}
-
-/* Private Functions */
+/* Constructors */
 
 static NODE *ast_create_constant(va_list args)
 {
@@ -62,4 +31,22 @@ static NODE *ast_create_string_literal(va_list args)
   const char *text = va_arg(args, char *);
   fprintf(stderr, "ast_create_string_literal: %s\n", text);
   return NULL;
+}
+
+NODE *ast_create(NODE_TYPE type, ...)
+{
+  va_list args;
+  NODE *result = NULL;
+  static NODE *(*create[])(va_list args) =
+    {
+      ast_create_identifier,
+      ast_create_constant,
+      ast_create_string_literal
+    };
+
+  va_start(args, type);
+  result = create[type](args);
+  va_end(args);
+
+  return result;
 }
