9589fcd39fca256168b0844d4a2cf7f557e45416
Samuel Tesla <samuel.tesla@gmail.com>
2008-10-28 17:31:43 -0500

    Implemented for loops.

--- src/ast_for.c
+++ src/ast_for.c
@@ -15,21 +15,42 @@ size_t ast_for_size() { return SLOT_SIZE; }
 
 static void find_literals(NODE *node, LITERALS *literals)
 {
-  ast_find_literals(S(node).body, literals);
+  if (S(node).body)
+    ast_find_literals(S(node).body, literals);
 }
 
 static void find_symbols(NODE *node, void *symbols)
 {
-  ast_find_symbols(S(node).initializer, symbols);
-  ast_find_symbols(S(node).condition, symbols);
+  if (S(node).initializer)
+    ast_find_symbols(S(node).initializer, symbols);
+  if (S(node).condition)
+    ast_find_symbols(S(node).condition, symbols);
   if (S(node).increment)
     ast_find_symbols(S(node).increment, symbols);
-  ast_find_symbols(S(node).body, symbols);
+  if (S(node).body)
+    ast_find_symbols(S(node).body, symbols);
 }
 
 static void generate_ir(NODE *node, IR *ir)
 {
-  /* TODO:IR */
+  int label_a = ir_make_label();
+  int label_b = ir_make_label();
+  if (S(node).initializer)
+    ast_generate_ir(S(node).initializer, ir);
+  ir_add(ir, IR_LABEL, IR_CONST, &label_a);
+  if (S(node).condition)
+    {
+      ast_generate_ir(S(node).condition, ir);
+      ir_add(ir, IR_IF_FALSE,
+             ast_ir_type(S(node).condition), ast_ir_value(S(node).condition),
+             IR_CONST, &label_b);
+    }
+  if (S(node).body)
+    ast_generate_ir(S(node).body, ir);
+  if (S(node).increment)
+    ast_generate_ir(S(node).increment, ir);
+  ir_add(ir, IR_JUMP, IR_CONST, &label_a);
+  ir_add(ir, IR_LABEL, IR_CONST, &label_b);
 }
 
 void print(NODE *node, FILE *out)
